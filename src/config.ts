dc9a2416d4df4bee4690257d36587e43ae9126da
PuBingA <1958440015@qq.com>
2026-01-14 20:27:02 +0800

    Complete my CV part

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'About Fubin Chen ';
+export const SITE_DESCRIPTION = 'Welcome to my personal space. A collection of my professional journey and the things I am passionate about.';
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
