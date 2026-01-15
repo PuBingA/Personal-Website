79845a57bd0557b5873042b5ad1625ba4921ae4e
PuBingA <1958440015@qq.com>
2026-01-15 17:49:15 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
+
 export default defineConfig({
   site: 'https://PuBingA.github.io',
   base: '/Personal-Website',
